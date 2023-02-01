e49ce26a68a09c5f6d5381318560ade678a0e03c
Matheus Silva <matheussilva.fga@gmail.com>
2023-02-01 13:57:14 -0300

    Creating Main Page component and styles

---
+++ src/Main/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+import { Platform, StatusBar } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0'};
+  background: #fafafa;
+`;
+
+export const CategoriesContainer = styled.View`
+  height: 80px;
+  margin-top: 32px;
+`;
+
+export const MenuContainer = styled.View`
+  flex: 1;
+`;
+
+export const Footer = styled.View`
+  min-height: 110px;
+  background: #fff;
+`;
+
+export const FooterContainer = styled.SafeAreaView``;
