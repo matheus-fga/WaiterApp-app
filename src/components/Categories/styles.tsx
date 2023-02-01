28af12eb80ddd2d5d86b4ade95e04a8de3f54933
Matheus Silva <matheussilva.fga@gmail.com>
2023-02-01 15:37:43 -0300

    Creating Categories component and styles

---
+++ src/components/Categories/styles.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
+
+export const Category = styled.TouchableOpacity`
+  align-items: center;
+  margin-left: 24px;
+`;
+
+export const Icon = styled.View`
+  justify-content: center;
+  align-items: center;
+  width: 44px;
+  height: 44px;
+  margin-bottom: 8px;
+  border-radius: 50px;
+  background: #fff;
+  box-shadow: 0px 2px 1px rgba(0, 0, 0, ${isAndroid ? 0.5 : 0.1});
+  elevation: 2;
+`;
